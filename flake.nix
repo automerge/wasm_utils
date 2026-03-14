dc43019977248a3f7eaeeee31914b248c884c693
Brooklyn Zelenka <hello@brooklynzelenka.com>
2026-03-14 15:56:46 -0700

    Imporve workflows

--- flake.nix
+++ flake.nix
@@ -4,17 +4,19 @@
   inputs = {
     nixpkgs.url = "nixpkgs/nixos-25.11";
     nixpkgs-unstable.url = "nixpkgs/nixpkgs-unstable";
+
+    command-utils.url = "git+https://codeberg.org/expede/nix-command-utils";
     flake-utils.url = "github:numtide/flake-utils";
 
     rust-overlay = {
       url = "github:oxalica/rust-overlay";
       inputs.nixpkgs.follows = "nixpkgs";
-      inputs.flake-utils.follows = "flake-utils";
     };
   };
 
   outputs = {
     self,
+    command-utils,
     flake-utils,
     nixpkgs,
     nixpkgs-unstable,
@@ -45,7 +47,6 @@
             "llvm-tools-preview"
             "rust-src"
             "rust-std"
-            "rustfmt"
           ];
 
           targets = [
@@ -59,13 +60,27 @@
           ];
         };
 
+        # Nightly rustfmt for unstable formatting options (imports_granularity, etc.)
+        # rustfmt links against librustc_driver; on macOS symlinks break @rpath
+        # resolution, so we wrap the binary with DYLD_LIBRARY_PATH.
+        nightly-rustfmt-unwrapped = pkgs.rust-bin.nightly.latest.minimal.override {
+          extensions = [ "rustfmt" ];
+        };
+
+        nightly-rustfmt = pkgs.writeShellScriptBin "rustfmt" ''
+          export DYLD_LIBRARY_PATH="${nightly-rustfmt-unwrapped}/lib''${DYLD_LIBRARY_PATH:+:$DYLD_LIBRARY_PATH}"
+          export LD_LIBRARY_PATH="${nightly-rustfmt-unwrapped}/lib''${LD_LIBRARY_PATH:+:$LD_LIBRARY_PATH}"
+          exec "${nightly-rustfmt-unwrapped}/bin/rustfmt" "$@"
+        '';
+
         format-pkgs = [
           pkgs.nixpkgs-fmt
           pkgs.alejandra
           pkgs.taplo
         ];
 
-        cargo-installs =  [
+        cargo-installs = [
+          pkgs.cargo-component
           pkgs.cargo-criterion
           pkgs.cargo-deny
           pkgs.cargo-expand
@@ -75,9 +90,28 @@
           pkgs.cargo-udeps
           pkgs.cargo-watch
           pkgs.twiggy
-          pkgs.cargo-component
           pkgs.wasm-bindgen-cli
           pkgs.wasm-tools
+        ];
+
+        # Built-in command modules from nix-command-utils
+        rust = command-utils.rust.${system};
+        wasm = command-utils.wasm.${system};
+        cmd = command-utils.cmd.${system};
+
+        command_menu = command-utils.commands.${system} [
+          # Rust commands
+          (rust.build { cargo = pkgs.cargo; })
+          (rust.test { cargo = pkgs.cargo; cargo-watch = pkgs.cargo-watch; })
+          (rust.lint { cargo = pkgs.cargo; })
+          (rust.fmt { cargo = pkgs.cargo; })
+          (rust.doc { cargo = pkgs.cargo; })
+          (rust.watch { cargo-watch = pkgs.cargo-watch; })
+
+          # Wasm commands
+          (wasm.build { wasm-pack = pkgs.wasm-pack; })
+          (wasm.test { wasm-pack = pkgs.wasm-pack; })
+          (wasm.doc { cargo = pkgs.cargo; xdg-open = pkgs.xdg-utils; })
         ];
 
       in rec {
@@ -86,8 +120,9 @@
 
           nativeBuildInputs = with pkgs;
             [
+              command_menu
+              nightly-rustfmt
               rust-toolchain
-              pkgs.irust
 
               http-server
               pkgs.binaryen
@@ -98,6 +133,12 @@
             ]
             ++ format-pkgs
             ++ cargo-installs;
+
+          shellHook = ''
+            export WORKSPACE_ROOT="$(pwd)"
+            export RUSTFMT="${nightly-rustfmt}/bin/rustfmt"
+            menu
+          '';
         };
 
         formatter = pkgs.alejandra;
